486d9054eab0685a9a34ce48f6c57567bec0d758
Michael Neely <michael@michaelneely.net>
2020-05-21 23:25:03 -0400

    Added more of the UI structure

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 var btn = document.getElementById('btn');
 
 btn.addEventListener('click', nextItem);
+var answers = {'correct':0, 'wrong': 0}
+var output = document.getElementById('output');
 
 function nextItem() {
     var url = 'https://opentdb.com/api.php?amount=10';
+    var html = '<h2>Question</h2>'
     requestAJAX(url, function(data) {
-        console.log(data.results);
+        console.log(data.results[0]);
+        var obj = data.results[0];
+        html += '<div><div class="cat">Category: '+obj.category+'</div>';
+        html += '<div class="question">'+obj.question+'</div></div>';
+        html += '<div></div>';
+        output.innerHTML = html;
+
     });
+}
+
+function questionBuilder() {
+    
 }
 
 function requestAJAX(url,callback) {
     var xhr = new XMLHttpRequest;
     xhr.onreadystatechange = function() {
         if(xhr.readyState == 4 && xhr.status == 200)
-        console.log(JSON.parse(xhr.responseText));
+        callback(JSON.parse(xhr.responseText));
     }
 
     xhr.open('GET',url,true);
